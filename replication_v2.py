333269fb0cd47fe61f459d22b443d287ff905e05
Md. Ehsan Shahmi <shahmi.ehsan@gmail.com>
2025-11-05 23:34:00 -0500

    started the second version

--- replication_v2.py
+++ replication_v2.py
@@ -1,1 +1,12 @@
-print ("this is just a test.")
+from datasets import load_dataset
+from google import genai
+from google.genai import types
+import pandas as pd
+import numpy as np
+import coverage as cv
+import os
+import subprocess
+from pathlib import Path
+import sys
+import unittest
+import io
